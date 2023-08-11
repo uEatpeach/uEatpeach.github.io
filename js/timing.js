d7d8a7cc431cdad69e7fe6e734fac37c2d6383ab
uEatpeach <2216109163@qq.com>
2023-08-11 17:54:56 +0800

    Site updated: 2023-08-11 17:54:53

--- js/timing.js
+++ js/timing.js
@@ -22,3 +22,33 @@ setInterval(function () {
         + tow(second) + '<span class="time">秒</span>';
     oSpan.innerHTML = "您已浏览网页" + str;
 }, 1000)
+
+
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i class="fa-fw fas fa-heart card-announcement-animation cc_pointer"></i> shadow');
+})
+
+// 本站运行时间
+$(document).ready(function(e){
+    $('.framework-info').html('本站已运行<SPAN id=span_dt_dt style="color: #fff;"></SPAN>');
+})
+
+function show_date_time(){
+window.setTimeout("show_date_time()", 1000);
+BirthDay=new Date("5/2/2020 0:0:0");
+today=new Date();
+timeold=(today.getTime()-BirthDay.getTime());
+sectimeold=timeold/1000
+secondsold=Math.floor(sectimeold);
+msPerDay=24*60*60*1000
+e_daysold=timeold/msPerDay
+daysold=Math.floor(e_daysold);
+e_hrsold=(e_daysold-daysold)*24;
+hrsold=Math.floor(e_hrsold);
+e_minsold=(e_hrsold-hrsold)*60;
+minsold=Math.floor((e_hrsold-hrsold)*60);
+seconds=Math.floor((e_minsold-minsold)*60);
+span_dt_dt.innerHTML=' <font style=color:#2d85f0>'+daysold+'</font> 天 <font style=color:#f4433c>'+hrsold+'</font> 时 <font style=color:#ffbc32>'+minsold+'</font> 分 <font style=color:#0aa858>'+seconds+'</font> 秒';
+}
+show_date_time();
+
